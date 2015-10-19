2745ed04a55c8cad155242db2bf3f87c81ebfbe3
Paul Adam Davis <pauladamdavis@gmail.com>
2015-10-20 00:50:13 +0100

    Optimise images, change RSS url

--- js/index.js
+++ js/index.js
@@ -8,31 +8,13 @@
         if ($(".post-content img").length > 0) {
             var content_width = parseInt($(".post-content").css("width"));
             $(".post-content").find("img").each(function(){
-                $(this).load(function(){
-                    if ($(this)[0].naturalWidth > content_width) {
-                        $(this).addClass("big");
-                    }
-                });
+                if ($(this)[0].naturalWidth > content_width) {
+                    $(this).addClass("big");
+                }
             });
         }
 
-        // Remove 'http://' from links with the .js-remove-domain-schema class
-        function no_schema_links () {
-            var links = document.querySelectorAll('.js-remove-domain-schema');
-            if (links) {
-                for (i = 0; i < links.length; ++i) {
-                    var link = links[i],
-                        text = link.innerHTML,
-                        no_schema = text.replace(/.*?:\/\//g, "");
-                    link.innerHTML = no_schema;
-                }
-            }
-        }
-        window.onload = function () {
-            no_schema_links();
-        }
-
-        // If a post, start Bigfoot.js
+        // If a post has footnotes, start Bigfoot.js
         if ($('.footnotes').length) {
             $.bigfoot(
                 {
