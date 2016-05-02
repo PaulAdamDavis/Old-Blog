710be875810ed99cd9da64f973ffc157c7790f64
PaulAdamDavis <pauladamdavis@gmail.com>
2016-05-02 01:30:18 +0100

    Make type more responsive and other minor enhancements

--- js/index.js
+++ js/index.js
@@ -7,11 +7,13 @@
         // Make images the ability to be wider than the content bleed out further
         if ($(".post-content img").length > 0) {
             var content_width = parseInt($(".post-content").css("width"));
-            $(".post-content").find("img").each(function(){
-                if ($(this)[0].naturalWidth > content_width) {
-                    $(this).addClass("big");
+
+            $('.post-content').imagesLoaded().progress( function( instance, image ) {
+                var result = image.isLoaded ? 'loaded' : 'broken';
+                if (image.img.naturalWidth > content_width) {
+                    $(image.img).addClass("big");
                 }
-            });
+              });
         }
 
         // If a post has footnotes, start Bigfoot.js
